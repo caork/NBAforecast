bc00fd3b215c4be57c58528c5b21433a76522904
caork <1565057559@qq.com>
2019-11-18 17:01:52 +0800

    new plot feature

--- backup.py
+++ backup.py
@@ -43,7 +43,7 @@ def flat(*data):
 
 
 
-def plot(*data, d2='line', d3='scatter'):
+def plot(*data, d2='line', d3='scatter', languages = 'en'):
     # 2d
     if d2 == 'hot':
         a = data[0]
@@ -51,6 +51,27 @@ def plot(*data, d2='line', d3='scatter'):
         plt.imshow(a)
         plt.colorbar()
         plt.show()
+    if d2 == 'radar':
+        if languages == 'zh':
+            plt.rcParams['font.sans-serif'] = ['KaiTi']
+        a = data[0]
+        b = data[1]
+        if type(a) == int:
+            a,b = b,a
+        a,b = flat(a,b)
+        datalenth = len(a)
+        labels = a
+        data_radar = b
+        angles = np.linspace(0,2*np.pi, datalenth, endpoint=False)
+        data_radar = np.concatenate((data_radar, [data_radar[0]]))
+        angles = np.concatenate((angles, [angles[0]]))
+        plt.polar(angles, data_radar, 'bo-', linewidth=1)
+        plt.thetagrids(angles * 180/np.pi, labels)
+        plt.fill(angles, data_radar, facecolor='r', alpha=0.25)
+        plt.ylim(0, max(b))
+        plt.show()
+
+
     elif len(data) != 3:
 
         if len(data) == 1:
@@ -76,8 +97,15 @@ def plot(*data, d2='line', d3='scatter'):
             for col in b:
                 plt.plot(a, col)
         else:
-
-            plt.plot(a, b)
+            if type(a) == int:
+                a,b = b,a
+                a,b = flat(a,b)
+            if d2 == 'bar':
+                plt.bar(a,b)
+            elif d2 == 'pie':
+                plt.pie(b,labels=a)
+            else:
+                plt.plot(a, b)
 
     # 3d
     elif len(data) == 3:
